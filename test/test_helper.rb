f6fe2df49df194039873f42ab14a998889426d8f
vagrant <vagrant@localhost.localdomain>
2019-01-13 08:52:15 +0900

    vagrant動作確認

---
+++ test/test_helper.rb
@@ -0,0 +1,17 @@
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+require 'rails/test_help'
+require "minitest/reporters"
+Minitest::Reporters.use!
+
+class ActiveSupport::TestCase
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  fixtures :all
+  include ApplicationHelper
+
+  def is_logged_in?
+    !!session[:user_id]
+  end
+
+  # Add more helper methods to be used by all tests here...
+end
