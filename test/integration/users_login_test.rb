2ff276c715c6e500c1afeff63d146fa86dd904e9
vagrant <vagrant@localhost.localdomain>
2019-01-14 06:29:01 +0900

    Add account activation

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -36,9 +36,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", user_path(@user), count: 0
   end
 
-  test "login with remmebering" do
+  test "login with remembering" do
     log_in_as(@user, remember_me: '1')
-    assert_equal cookies['remember_token'], assigns(:user).remember_token
     assert_not_empty cookies['remember_token']
   end
 
